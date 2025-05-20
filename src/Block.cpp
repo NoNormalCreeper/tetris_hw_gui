700826e25b8944c747a5f5161c1ae9ea284860df
Rikka <2560359315@qq.com>
2025-05-20 23:45:40 +0800

    feat: add rotate method and modify getBlockByLabel to be static

--- src/Block.cpp
+++ src/Block.cpp
@@ -11,11 +11,22 @@ namespace k_Block {
 }
 
 
-const Block& Block::getBlockByLabel(const char label) const {
+const Block& Block::getBlockByLabel(const char label) {
     for (const auto &block : k_Block::list) {
         if (block.label == label) {
             return block;
         }
     }
     throw std::runtime_error("Block label '" + std::to_string(label) + "' not found");
+}
+
+const Block& Block::rotate() const {
+    std::vector<Pos> new_occupied;
+    new_occupied.reserve(this->occupied.size());
+
+    for (const auto &pos: this->occupied) {
+        new_occupied.emplace_back(this->anchor.x + (pos.y - this->anchor.y), this->anchor.y + (this->anchor.x - pos.x));
+    }
+
+    return Block(this->label, this->color, new_occupied, this->anchor);
 }
--- src/Block.h
+++ src/Block.h
@@ -16,10 +16,11 @@ public:
     std::vector<Pos> occupied; // 占用的坐标
     Pos anchor; // 旋转锚点坐标
 
-    Block(const char label, QString color, std::vector<Pos> occupied, Pos anchor = Pos(1, 0))
-        : label(label), color(std::move(color)), occupied(std::move(occupied)), anchor(std::move(anchor)) {}
+    Block(const char label, QString color, std::vector<Pos> occupied, const Pos anchor = Pos(1, 0))
+        : label(label), color(std::move(color)), occupied(std::move(occupied)), anchor(anchor) {}
 
-    const Block& getBlockByLabel(const char label) const;
+    static const Block& getBlockByLabel(char label);
+    const Block& rotate() const;    // 返回顺时针旋转90度的新方块
 };
 
 
